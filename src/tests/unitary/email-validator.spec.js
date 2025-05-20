975fea0301dfeb5bb3d054137244af97cdad6428
Vinny Daros Trindade <122296481+vinnydarostrindad@users.noreply.github.com>
2025-05-20 19:51:47 -0300

    ensure `EmailValidator` returns false if email is invalid
    unitary/refactoring

--- src/tests/unitary/email-validator.spec.js
+++ src/tests/unitary/email-validator.spec.js
@@ -4,10 +4,16 @@ const makeSut = () => {
   return new EmailValidator();
 };
 
-describe("Email Valid", () => {
+describe("Email Validation", () => {
   test("Should return true if email is valid", () => {
     const sut = makeSut();
     const isValid = sut.execute("valid_email@mail.com");
     expect(isValid).toBe(true);
+  });
+
+  test("Should return false if email is invalid", () => {
+    const sut = makeSut();
+    const isValid = sut.execute("invalid_email.com");
+    expect(isValid).toBe(false);
   });
 });
