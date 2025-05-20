92731bfbd223d161eca8bddb94f4601a52bc8f0d
Vinny Daros Trindade <122296481+vinnydarostrindad@users.noreply.github.com>
2025-05-19 21:55:35 -0300

    test: ensure `RegisterBusinessRouter` returns 400 if no name, email or password is provided
    Starting a series of refactors while implementing unit tests. During this process, I’ll use the description `unitary/refactoring`. Some things may break temporarily, including integration tests, until the full refactor is complete.

--- src/presentation/register-business-router.js
+++ src/presentation/register-business-router.js
@@ -1,30 +1,69 @@
-import registerBusinessUseCase from "../domain/usecase/register-business-usecase.js";
+// import registerBusinessUseCase from "../domain/usecase/register-business-usecase.js";
+import MissingParamError from "../utils/errors/missing-param-error.js";
 
-async function registerBusinessController(req, res, method) {
-  if (method === "POST") {
-    let body = "";
+export default class RegisterBusinessRouter {
+  route(httpRequest) {
+    const { name, email, password } = httpRequest.body;
 
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-
-    req.on("end", async () => {
-      const { name, email, password } = JSON.parse(body);
-      if (!name || !email || !password) {
-        throw new Error("Preencha todos os campos");
-      }
-
-      try {
-        let response = await registerBusinessUseCase(name, email, password);
-        res.writeHead(201, { "content-type": "application/json" });
-        return res.end(JSON.stringify(response));
-      } catch (err) {
-        console.log(err);
-        res.writeHead(400);
-        return res.end("Preencha todos os campos corretamente");
-      }
-    });
+    if (!name) {
+      return {
+        statusCode: 400,
+        body: new MissingParamError("name"),
+      };
+    }
+    if (!email) {
+      return {
+        statusCode: 400,
+        body: new MissingParamError("email"),
+      };
+    }
+    if (!password) {
+      return {
+        statusCode: 400,
+        body: new MissingParamError("password"),
+      };
+    }
   }
 }
 
-export default registerBusinessController;
+// async function registerBusinessRouter(req, res, method) {
+//   const { name, email, password } = JSON.parse(body);
+//   if (!name || !email || !password) {
+//     throw new Error("Preencha todos os campos");
+//   }
+
+//   try {
+//     let response = await registerBusinessUseCase(name, email, password);
+//     res.writeHead(201, { "content-type": "application/json" });
+//     return res.end(JSON.stringify(response));
+//   } catch (err) {
+//     console.log(err);
+//     res.writeHead(400);
+//     return res.end("Preencha todos os campos corretamente");
+//   }
+
+// if (method === "POST") {
+//   let body = "";
+
+//   req.on("data", (chunk) => {
+//     body += chunk.toString();
+//   });
+
+//   req.on("end", async () => {
+//     const { name, email, password } = JSON.parse(body);
+//     if (!name || !email || !password) {
+//       throw new Error("Preencha todos os campos");
+//     }
+
+//     try {
+//       let response = await registerBusinessUseCase(name, email, password);
+//       res.writeHead(201, { "content-type": "application/json" });
+//       return res.end(JSON.stringify(response));
+//     } catch (err) {
+//       console.log(err);
+//       res.writeHead(400);
+//       return res.end("Preencha todos os campos corretamente");
+//     }
+//   });
+// }
+// }
---
+++ src/tests/unitary/register-business-router.spec.js
@@ -0,0 +1,43 @@
+import RegisterBusinessRouter from "../../presentation/register-business-router";
+import MissingParamError from "../../utils/errors/missing-param-error";
+
+describe("Register Business Router", () => {
+  test("Should return 400 if no name is provided", () => {
+    const sut = new RegisterBusinessRouter();
+    const httpRequest = {
+      body: {
+        email: "any_email@mail.com",
+        password: "any_password",
+      },
+    };
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("name"));
+  });
+
+  test("Should return 400 if no email is provided", () => {
+    const sut = new RegisterBusinessRouter();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        password: "any_password",
+      },
+    };
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("email"));
+  });
+
+  test("Should return 400 if no password is provided", () => {
+    const sut = new RegisterBusinessRouter();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@mail.com",
+      },
+    };
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("password"));
+  });
+});
