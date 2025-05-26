24efb133eabf1053aa6a7f2de0215f3247f74625
Vinny Daros Trindade <122296481+vinnydarostrindad@users.noreply.github.com>
2025-05-26 16:12:07 -0300

    move client connection to a separate method
    unitary/refactoring

--- src/infra/adaptors/postgres-adapter.js
+++ src/infra/adaptors/postgres-adapter.js
@@ -13,21 +13,26 @@ export default {
     let client;
 
     try {
-      client = new Client({
-        user: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-      });
-
-      await client.connect();
+      client = await this.getNewClient();
       await client.query(queryObject);
     } catch (err) {
       console.log(err);
     } finally {
       await client?.end();
     }
+  },
+
+  async getNewClient() {
+    const client = new Client({
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+    });
+
+    await client.connect();
+    return client;
   },
 };
 
