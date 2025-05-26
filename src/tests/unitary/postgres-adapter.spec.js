b8d499f8fa5b511be6b5a08de21d1e4750ceb95e
Vinny Daros Trindade <122296481+vinnydarostrindad@users.noreply.github.com>
2025-05-26 16:25:35 -0300

    enhance `postgres-adaptor` tests
    unitary/refactoring

--- src/tests/unitary/postgres-adapter.spec.js
+++ src/tests/unitary/postgres-adapter.spec.js
@@ -17,6 +17,7 @@ jest.unstable_mockModule("pg", () => ({
 
     static async query(queryObject) {
       Client.query.queryObject = queryObject;
+      return "any_object";
     }
 
     static async end() {
@@ -30,55 +31,59 @@ const sut = (await import("../../infra/adaptors/postgres-adapter.js")).default;
 const { Client } = await import("pg");
 
 describe("Postgres Adapter", () => {
-  test("Should throw if no queryObject is provided", async () => {
-    const promise = sut.query();
-    expect(promise).rejects.toThrow(new MissingParamError("queryObject"));
-  });
+  describe("query Method", () => {
+    test("Should throw if no queryObject is provided", async () => {
+      const promise = sut.query();
+      expect(promise).rejects.toThrow(new MissingParamError("queryObject"));
+    });
 
-  test("Should call Client with correct data", async () => {
-    const queryObject = {
-      text: "any_query",
-      values: ["any_value"],
-    };
-    await sut.query(queryObject);
-    expect(Client.connectionData).toEqual({
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
+    test("Should call client.query with correct object", async () => {
+      const queryObject = {
+        text: "any_query",
+        values: ["any_value"],
+      };
+      await sut.query(queryObject);
+      expect(Client.query.queryObject).toEqual(queryObject);
+    });
+
+    test("Should call client.end correctly", async () => {
+      const queryObject = {
+        text: "any_query",
+        values: ["any_value"],
+      };
+      await sut.query(queryObject);
+      expect(Client.end.connectionEnded).toBe(true);
     });
   });
 
-  test("Should call client.connect", async () => {
-    const queryObject = {
-      text: "any_query",
-      values: ["any_value"],
-    };
-    await sut.query(queryObject);
-    expect(Client.connect.isConnected).toBe(true);
-  });
+  describe("getNewClient Method", () => {
+    test("Should call Client with correct data", async () => {
+      const queryObject = {
+        text: "any_query",
+        values: ["any_value"],
+      };
+      await sut.query(queryObject);
+      expect(Client.connectionData).toEqual({
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+      });
+    });
 
-  test("Should call client.query with correct object ", async () => {
-    const queryObject = {
-      text: "any_query",
-      values: ["any_value"],
-    };
-    await sut.query(queryObject);
-    expect(Client.query.queryObject).toEqual(queryObject);
-  });
+    test("Should call client.connect correctly", async () => {
+      const queryObject = {
+        text: "any_query",
+        values: ["any_value"],
+      };
+      await sut.query(queryObject);
+      expect(Client.connect.isConnected).toBe(true);
+    });
 
-  test("Should call client.end", async () => {
-    const queryObject = {
-      text: "any_query",
-      values: ["any_value"],
-    };
-    await sut.query(queryObject);
-    expect(Client.end.connectionEnded).toBe(true);
-  });
-
-  test("Should call getNewClient and return the client already connected", async () => {
-    await sut.getNewClient();
-    expect(Client.connect.isConnected).toBe(true);
+    test("Should return the client already connected", async () => {
+      await sut.getNewClient();
+      expect(Client.connect.isConnected).toBe(true);
+    });
   });
 });
