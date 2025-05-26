2261a5a11933a6348f59144ac7397b98dc3b4b71
Vinny Daros Trindade <122296481+vinnydarostrindad@users.noreply.github.com>
2025-05-26 14:38:47 -0300

    ensure `postgresAdapter` calls `Client`with correct data
    unitary/refactoring

--- src/infra/adaptors/postgres-adapter.js
+++ src/infra/adaptors/postgres-adapter.js
@@ -1,15 +1,26 @@
+import { Client } from "pg";
 import MissingParamError from "../../utils/errors/missing-param-error";
+
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.development" });
 
 export default {
   async query(queryObject) {
     if (!queryObject) {
       throw new MissingParamError("queryObject");
     }
+
+    const client = new Client({
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+    });
   },
 };
 
 // import { Client } from "pg";
-
 // import dotenv from "dotenv";
 // dotenv.config({ path: ".env.development" });
 
--- src/tests/unitary/postgres-adapter.spec.js
+++ src/tests/unitary/postgres-adapter.spec.js
@@ -1,9 +1,36 @@
-import sut from "../../infra/adaptors/postgres-adapter.js";
+import { jest } from "@jest/globals";
+
+jest.unstable_mockModule("pg", () => ({
+  Client: class {
+    constructor(connectionData) {
+      Client.connectionData = connectionData;
+      return Client.client;
+    }
+  },
+}));
+
 import MissingParamError from "../../utils/errors/missing-param-error.js";
+const sut = (await import("../../infra/adaptors/postgres-adapter.js")).default;
+const { Client } = await import("pg");
 
 describe("Postgres Adapter", () => {
   test("Should throw if no queryObject is provided", async () => {
     const promise = sut.query();
     expect(promise).rejects.toThrow(new MissingParamError("queryObject"));
+  });
+
+  test("Should call Client with correct data", async () => {
+    const queryObject = {
+      text: "any_query",
+      values: ["any_value"],
+    };
+    await sut.query(queryObject);
+    expect(Client.connectionData).toEqual({
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+    });
   });
 });
