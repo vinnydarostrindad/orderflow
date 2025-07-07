7dde9c0d069205f68662a2d8054af63b28b1b6f6
Vinny Daros Trindade <122296481+vinnydarostrindad@users.noreply.github.com>
2025-07-07 19:43:35 -0300

    improve business register page and create `default.css`

--- src/main/pages/script.js
+++ src/main/pages/script.js
@@ -1,25 +1,64 @@
-document.forms[0].onsubmit = async (e) => {
+const registerForm = document.querySelector(".register-form");
+const submitBtn = document.querySelector("#submitBtn");
+const snackbar = document.querySelector(".snackbar");
+
+let timeoutId;
+
+registerForm.addEventListener("submit", submitForm);
+
+function showErrorSnackBar() {
+  if (timeoutId) clearTimeout(timeoutId);
+
+  snackbar.innerHTML =
+    "<p>Erro ao fazer o registro. <br /> Tente novamente.</p>";
+  snackbar.classList.remove("snackbar--hidden");
+  snackbar.setAttribute("aria-hidden", "false");
+
+  timeoutId = setTimeout(() => {
+    snackbar.classList.add("snackbar--hidden");
+    snackbar.setAttribute("aria-hidden", "true");
+    snackbar.addEventListener(
+      "transitionend",
+      () => {
+        snackbar.innerHTML = "";
+      },
+      { once: true },
+    );
+  }, 5000);
+}
+
+async function submitForm(e) {
   e.preventDefault();
+
+  submitBtn.disabled = true;
+  submitBtn.innerHTML = "Registrando <div class='loading'></div>";
 
   const name = document.querySelector("#name").value;
   const email = document.querySelector("#email").value;
   const password = document.querySelector("#password").value;
 
-  const response = await fetch("http://localhost:3000/api/v1/business", {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({
-      name,
-      email,
-      password,
-    }),
-  });
+  try {
+    const response = await fetch("http://localhost:3000/api/v1/business", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
 
-  const responseBody = await response.json();
+    console.log(response.ok);
+    const responseBody = await response.json();
+    console.log(responseBody);
 
-  console.log(responseBody);
-
-  window.location.href = `http://localhost:5500/src/main/pages/register_employee/index.html?businessId=${responseBody.business.id}`;
-};
+    window.location.href = `http://localhost:5500/src/main/pages/register_employee/index.html?b=${responseBody.business.id}`;
+  } catch (err) {
+    console.error(err);
+    showErrorSnackBar();
+    submitBtn.textContent = "Registrar";
+    submitBtn.disabled = false;
+  }
+}
