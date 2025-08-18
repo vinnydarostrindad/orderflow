e9d57b0d035be65f8ae4a66b70ade1cc73807018
Vinny Daros Trindade <122296481+vinnydarostrindad@users.noreply.github.com>
2025-08-18 07:25:49 -0300

    create `verify` method in `jwt.js`

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,17 +1,33 @@
 import jwt from "jsonwebtoken";
 import MissingParamError from "./errors/missing-param-error.js";
 import DependencyError from "./errors/dependency-error.js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.development" });
+
+const SECRET = process.env.SECRET;
 
 const jsonWebToken = {
-  sign(payload, secret) {
+  sign(payload) {
     if (!payload) throw new MissingParamError("payload");
-    if (!secret) throw new MissingParamError("secret");
 
     try {
-      return jwt.sign(payload, secret);
+      return jwt.sign(payload, SECRET);
     } catch (error) {
       throw new DependencyError("jsonWebToken.sign", {
         message: "Failed to generate token",
+        cause: error,
+      });
+    }
+  },
+  verify(token) {
+    if (!token) throw new MissingParamError("token");
+
+    try {
+      return jwt.verify(token, SECRET);
+    } catch (error) {
+      throw new DependencyError("jsonWebToken.verify", {
+        message: "Failed to verify token",
         cause: error,
       });
     }
