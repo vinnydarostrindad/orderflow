48dc1e36112dc92813c623ce377724b046c1238d
Vinny Daros Trindade <122296481+vinnydarostrindad@users.noreply.github.com>
2025-08-18 06:58:40 -0300

    create `authMiddlewares` and `get-cookies` helper

---
+++ src/main/middlewares/authMiddleware.js
@@ -0,0 +1,46 @@
+import getCookies from "../helper/get-cookies.js";
+import jsonWebToken from "../../utils/jwt.js";
+
+function authMiddleware(req, routeInfoObj) {
+  if (!routeInfoObj) return null;
+
+  const cookies = getCookies(req.headers.cookie);
+
+  if (!cookies) {
+    if (!routeInfoObj.role) {
+      return {
+        auth: true,
+        empmloyeeData: {},
+      };
+    }
+
+    return {
+      auth: false,
+      employeeData: {},
+    };
+  }
+
+  const token = cookies.token;
+  if (token) {
+    const employeeData = jsonWebToken.verify(token);
+    if (!routeInfoObj.role) {
+      return {
+        auth: true,
+        employeeData,
+      };
+    }
+
+    if (employeeData.role === routeInfoObj.role) {
+      return {
+        auth: true,
+        employeeData,
+      };
+    }
+    return {
+      auth: false,
+      employeeData,
+    };
+  }
+}
+
+export default authMiddleware;
