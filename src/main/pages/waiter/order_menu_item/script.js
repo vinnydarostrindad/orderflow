4e7d4ce320db5027c6f41cf24b06ae1ecf8c5ff2
Vinny Daros Trindade <122296481+vinnydarostrindad@users.noreply.github.com>
2025-09-11 13:06:52 -0300

    create script for `order_menu_item` page

--- src/main/pages/waiter/order_menu_item/script.js
+++ src/main/pages/waiter/order_menu_item/script.js
@@ -2,47 +2,180 @@ import "/waiter/components/nav/script.js";
 import "/waiter/components/menu-card.js";
 import "/components/header/script.js";
 import "/components/snackbar.js";
+import supabase from "/scripts/supabase.js";
 import API_URL from "/scripts/config-api-url.js";
 
-const menusContainer = document.querySelector(".menus");
+const pathParts = window.location.pathname.split("/");
+const menuId = pathParts[2];
+const menuItemId = pathParts[4];
+const DESCRIPTION_COLLAPSE_HEIGHT = 60;
+let itemPrice;
+
 const snackbar = document.querySelector("#snackbar");
+const nameEl = document.querySelector(".menu-item__name");
+const typeEl = document.querySelector(".menu-item__type");
+const priceEl = document.querySelector(".menu-item__price");
+const imgBox = document.querySelector(".menu-item__img-container");
+const descEl = document.querySelector(".menu-item__description");
+const descriptionHider = document.querySelector(".description-hider");
+const showMoreDescriptionBtn = document.querySelector(
+  ".menu-item__description-btn",
+);
+const descBtn = document.querySelector(".menu-item__description-btn");
+const ingredientsBox = document.querySelector(".menu-item__ingredients");
+const ingredientsList = ingredientsBox.querySelector("ul");
+const waitingEl = document.querySelector(".menu-item__waiting-time");
+const orderItemBtn = document.querySelector("#orderItemBtn");
 
-menusContainer.addEventListener("click", redirectToMenu);
+orderItemBtn.addEventListener("click", postOrder);
 
-function redirectToMenu(e) {
-  const menuCard = e.target.closest("menu-card");
-  if (!menuCard) return;
+async function postOrder() {
+  const tableId = sessionStorage.getItem("tableId");
+  const tableNumber = sessionStorage.getItem("tableNumber");
 
-  const menuId = menuCard.dataset.id;
-  if (!menuId) throw new Error("Cadê a porcaira do iID IDIOTA");
+  let orderBody;
+  try {
+    const orderResponse = await fetch(
+      `${API_URL}/api/v1/table/${tableId}/order`,
+      {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ tableNumber }),
+      },
+    );
 
-  window.location.href = `/menu/${menuId}`;
-}
+    if (!orderResponse.ok) {
+      throw {
+        status: orderResponse.status,
+        statusText: orderResponse.statusText,
+        url: orderResponse.url,
+      };
+    }
 
-function addMenusToHTML(menus) {
-  if (menus.length === 0) {
-    menusContainer.innerHTML = "<p>Nenhum cardápio foi criado</p>";
-    return;
+    orderBody = await orderResponse.json();
+  } catch (error) {
+    console.error(error);
+    snackbar.show(
+      "error",
+      "<p>Erro ao tentar realizar pedido <br> Tente novamente.</p>",
+    );
   }
 
-  const menusHTML = menus
-    .map((menu) => {
-      return `
-      <menu-card
-        name="${menu.name}"
-        imgPath="/waiter/menus/img/menu-photo.jpeg"
-        data-id="${menu.id}"
-      ></menu-card>
-    `;
-    })
-    .join("");
+  const quantity = document.querySelector("#quantity").value;
+  const notes = document.querySelector("#notes").value;
 
-  menusContainer.innerHTML = menusHTML;
+  try {
+    const orderItemResponse = await fetch(
+      `${API_URL}/api/v1/table/${tableId}/order/${orderBody.id}/item`,
+      {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({
+          menuItemId,
+          quantity,
+          unitPrice: itemPrice,
+          totalPrice: itemPrice * quantity,
+          notes,
+        }),
+      },
+    );
+
+    if (!orderItemResponse.ok) {
+      throw {
+        status: orderItemResponse.status,
+        statusText: orderItemResponse.statusText,
+        url: orderItemResponse.url,
+      };
+    }
+
+    const orderItemBody = await orderItemResponse.json();
+    console.log("FINAL:", orderItemBody);
+    window.location.href = `/menu/${menuId}`;
+  } catch (error) {
+    console.error(error);
+    snackbar.show(
+      "error",
+      "<p>Erro ao tentar realizar pedido <br> Tente novamente.</p>",
+    );
+  }
 }
 
-async function getMenus() {
+function populateMenuItem({
+  price,
+  name,
+  ingredients,
+  description,
+  imagePath,
+  type,
+  waitingTime,
+}) {
+  nameEl.textContent = name;
+
+  type ? (typeEl.textContent = type) : (typeEl.style.display = "none");
+
+  const formattedPrice = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(price);
+  priceEl.textContent = formattedPrice;
+
+  if (imagePath) {
+    const imgEl = document.createElement("img");
+    const { publicUrl } = supabase.getUrl("menu-items-img", imagePath);
+    imgEl.src = publicUrl;
+    imgBox.prepend(imgEl);
+  } else {
+    imgBox.style.display = "none";
+    priceEl.style.position = "static";
+    nameEl.insertAdjacentElement("afterend", priceEl);
+    document
+      .querySelector(".menu-item__header")
+      .classList.add("menu-item__header--no-img");
+  }
+
+  description
+    ? descEl.prepend(description)
+    : (descEl.style.display = descBtn.style.display = "none");
+
+  ingredients?.length
+    ? ingredients.forEach((ingredient) => {
+        const li = document.createElement("li");
+        li.className = "menu-item__ingredient";
+        li.textContent = ingredient;
+        ingredientsList.append(li);
+      })
+    : (ingredientsBox.style.display = "none");
+
+  waitingTime
+    ? (waitingEl.querySelector("span").textContent = waitingTime)
+    : (waitingEl.style.display = "none");
+
+  document.querySelector(".menu-item--skeleton").remove();
+  document.querySelector(".menu-item").removeAttribute("hidden");
+
+  if (descEl.clientHeight < DESCRIPTION_COLLAPSE_HEIGHT) {
+    descriptionHider.remove();
+    showMoreDescriptionBtn.remove();
+  } else {
+    showMoreDescriptionBtn.addEventListener("click", () => {
+      descriptionHider.classList.toggle("description-hider--hidden");
+      if (descriptionHider.classList.contains("description-hider--hidden")) {
+        descEl.style.maxHeight = descEl.scrollHeight + "px";
+        showMoreDescriptionBtn.textContent = "Mostrar menos";
+      } else {
+        descEl.style.maxHeight = "4rem";
+        showMoreDescriptionBtn.textContent = "Mostrar mais";
+      }
+    });
+  }
+}
+
+async function getMenuItem() {
   try {
-    const response = await fetch(`${API_URL}/api/v1/menu`);
+    const response = await fetch(
+      `${API_URL}/api/v1/menu/${menuId}/item/${menuItemId}`,
+    );
+
     if (!response.ok) {
       throw {
         status: response.status,
@@ -51,16 +184,16 @@ async function getMenus() {
       };
     }
 
-    const menus = await response.json();
-
-    addMenusToHTML(menus);
+    const menuItem = await response.json();
+    itemPrice = menuItem.price;
+    populateMenuItem(menuItem);
   } catch (error) {
     console.error(error);
     snackbar.show(
       "error",
-      "<p>Erro ao tentar obter cardápios <br> Tente novamente.</p>",
+      "<p>Erro ao tentar obter item <br> Tente novamente.</p>",
     );
   }
 }
 
-getMenus();
+getMenuItem();
--- src/tests/integration/order/register-order.test.js
+++ src/tests/integration/order/register-order.test.js
@@ -75,35 +75,4 @@ describe("POST /api/v1/business/[businessId]/table/[tableId]/order", () => {
     expect(typeof order.updated_at).toBe("string");
     expect(Date.parse(order.updated_at)).not.toBeNaN();
   });
-
-  test("Should return ValidationError if number already exists in business", async () => {
-    const { table, token } = await makeOrderTestContext();
-
-    const requestBody = {
-      tableNumber: 1,
-    };
-
-    const response = await fetch(
-      `http://localhost:3000/api/v1/table/${table.id}/order`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          cookie: `token=${token}`,
-        },
-        body: JSON.stringify(requestBody),
-      },
-    );
-
-    const responseBody = await response.json();
-
-    expect(response.status).toBe(400);
-
-    expect(responseBody).toEqual({
-      name: "ValidationError",
-      message: "The table number provided is already in use.",
-      action: "Use another table number to perform this operation.",
-      statusCode: 400,
-    });
-  });
 });
