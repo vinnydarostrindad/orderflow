171d677bfef0481dbd7a23328103e91ef7cde207
Vinny Daros Trindade <122296481+vinnydarostrindad@users.noreply.github.com>
2025-09-16 08:49:20 -0300

    move text stats logic to separate script

--- src/main/pages/manager/dashboard/script.js
+++ src/main/pages/manager/dashboard/script.js
@@ -1,6 +1,7 @@
 import "/components/snackbar.js";
 import "/components/header/script.js";
 import API_URL from "/scripts/config-api-url.js";
+import textStats from "/manager/dashboard/js/text-stats.js";
 import { makeChart, updateChart } from "/manager/dashboard/js/chart.js";
 import {
   showLastOrders,
@@ -10,11 +11,6 @@ import {
 const headerMenuBtn = document.querySelector("#headerMenuBtn");
 const navBar = document.querySelector("#navBar");
 const timeFilterSelect = document.querySelector(".business-stats__select");
-// const revenue = document.querySelector("#revenue");
-// const hourlyRevenue = document.querySelector("#hourlyRevenue");
-const ordersAmount = document.querySelector("#ordersAmount");
-const completedOrder = document.querySelector("#completedOrders");
-const canceledOrder = document.querySelector("#canceledOrders");
 const snackbar = document.querySelector("#snackbar");
 
 let orderedItems;
@@ -70,55 +66,11 @@ async function fetchFilteredOrderedItems(e) {
   // const filteredOrderedItems = await fetch("http:///localhost:3000/api/v1/business/id/orders?time=week")
 }
 
-// TextStats funcitons
-
-// async function showRevenue() {
-// Quando fizer a API dos pagamentos, finalizar essa função
-// const revenue = await fetch("${API_URL}/api/v1/business/id/payments")
-// Fazer os calculos necessários
-// revenue.textContent = revenueCalculated
-//
-// showHourlyRevenue(revenueCalclated)
-// }
-
-// function showHourlyRevenue(revenue) {
-// Quando fizer a API dos pagamentos, finalizar essa função
-// hourlyRevenue.textContent = revenue / 24; Talvez;
-// }
-
-function showAmountOfOrders() {
-  const amountOfOrders = orderedItems.reduce((acc, item) => {
-    return acc + +item.quantity;
-  }, 0);
-
-  ordersAmount.textContent = amountOfOrders;
-}
-
-function showAmountOfOrdersCompleted() {
-  const ordersCompleted = orderedItems.reduce((acc, item) => {
-    if (item.status === "delivered") return acc++;
-    return acc;
-  }, 0);
-
-  completedOrder.textContent = ordersCompleted;
-}
-
-function showAmountOfOrdersCanceled() {
-  const ordersCanceled = orderedItems.reduce((acc, item) => {
-    if (item.status === "canceled") return acc++;
-    return acc;
-  }, 0);
-
-  canceledOrder.textContent = ordersCanceled;
-}
-
 orderedItems = await fetchOrderedItems();
 orderedMenuItems = orderedItems.length > 0 ? await fetchOrderedMenuItems() : [];
 const lastFetchTime = Date.now();
 
-showAmountOfOrders();
-showAmountOfOrdersCompleted();
-showAmountOfOrdersCanceled();
+textStats(orderedItems);
 makeChart(orderedItems, orderedMenuItems, lastFetchTime);
 showLastOrders(orderedItems, orderedMenuItems);
 
@@ -127,9 +79,7 @@ setInterval(async () => {
   orderedMenuItems =
     orderedItems.length > 0 ? await fetchOrderedMenuItems() : [];
 
-  showAmountOfOrders();
-  showAmountOfOrdersCompleted();
-  showAmountOfOrdersCanceled();
+  textStats(orderedItems);
   updateChart(orderedItems, orderedMenuItems);
   updateLastOrders(orderedItems, orderedMenuItems);
   console.log("FOI, legal");
